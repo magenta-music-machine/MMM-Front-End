f46f0b474c3e69fb72469c75e30e55f98cb652ae
Alfredo <fredy_rios0123@outlook.com>
2022-09-06 17:12:12 -0700

    Made a navbar

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ class Footer extends React.Component {
   render() {
     return (
       <>
-        <footer>
+        <footer class='copyright'>
           Copyright &copy;{''} Magenta Music Machine
         </footer>
       </>
