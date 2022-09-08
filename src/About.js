802ecd866f99be0f94b15d2f21fa9ee1fe3534d9
Alfredo <fredy_rios0123@outlook.com>
2022-09-08 16:17:37 -0700

    Fixed the about us page

--- src/About.js
+++ src/About.js
@@ -11,50 +11,50 @@ class About extends React.Component {
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="../Images/Adrian.jpg"
+            src="../Adrian.jpg"
             alt="First slide"
           />
           <Carousel.Caption>
             <h3>Adrian Cosme-Halverson</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <p>Fire support specialist 4+ years of honorable military service. Seasoned individual with professional work experience in a variety of dynamic and fast-paced environments. Experience in a multitude of work environments form professional office experience to months long austere environment training.</p>
           </Carousel.Caption>
         </Carousel.Item>
 
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="../Images/David.jpg"
+            src="../David.jpg"
             alt="Second slide"
           />
           <Carousel.Caption>
             <h3>Hanbyeol (David) Lee</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <p>Medical specialist with 4+ years of honorable military service.</p>
           </Carousel.Caption>
         </Carousel.Item>
 
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="../Images/Jordan.jpg"
+            src="../Jordan.jpg"
             alt="Third slide"
           />
 
           <Carousel.Caption>
             <h3>Jordan Yamada</h3>
             <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              Air traffic control 5+ years of honorable military service. Experience in troubleshooting, time management, and risk management.
             </p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="../Images/Alfredo.jpg"
+            src="../Alfredo.jpg"
             alt="First slide"
           />
           <Carousel.Caption>
             <h3>Alfredo Orquiz</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <p>Helicopter Airframe Mechanic 5+ years of honorable military service. Experience in operation risk management, military missions by managing leadership, communications, training, operations, logistics, compliance, and administration for over ten workers.</p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
