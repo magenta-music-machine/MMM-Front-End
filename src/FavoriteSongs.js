bc93686e21b7a782618d1c243d28bac8a5615b2d
Adrian Cosme-Halverson <adrian.cosme5850@gmail.com>
2022-09-08 16:26:18 -0700

    favSongs

--- src/FavoriteSongs.js
+++ src/FavoriteSongs.js
@@ -1,11 +1,36 @@
+import axios from 'axios';
 import React from 'react';
 import Button from 'react-bootstrap/Button'
 
 class FavoriteSongs extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            art: ''
+        }
+    }
+
+getAlbumArt = async () => {
+    let art = [];
+    let response = this.props.favSongs.map(song => `http://api.napster.com/v2.2/albums/${song.albumId}/images?apikey=`)
+    let responseArt = await axios.all(response.map((URL) => axios.get(URL)))
+    console.log(responseArt)
+    responseArt.forEach(data => art.push(data.data.images[0].url))
+    console.log(art)
+    this.setState ({
+        art: art
+    })
+}
+
+componentDidMount(){
+    this.getAlbumArt();
+}
+
     favSongsListGenerator = () => {
-        let favSongsList = this.props.favSongs.map(song => {
-            console.log(song)
-            return <><p>{song.artist}</p>
+        let favSongsList = this.props.favSongs.map((song, index) => {
+            return <>
+            <img src={this.state.art[index]} alt='Album cover'/>
+            <p>{song.artist}</p>
             <div>{song.title}</div>
             <Button onClick= {() => {this.props.deleteSong(song._id)}}>X</Button>
             </>
